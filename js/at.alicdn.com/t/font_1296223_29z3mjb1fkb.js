73558d2496600f9ac26b9d0a46d152310620a271
sue <3117169940@qq.com>
2020-04-03 20:37:20 +0800

    loveMooc

---
+++ js/at.alicdn.com/t/font_1296223_29z3mjb1fkb.js
@@ -0,0 +1,46 @@
+!
+    function(c) {
+        var t, n = '<svg><symbol id="icon-camera" viewBox="0 0 1025 1024"><path d="M341.34016 85.34016l341.34016 0 85.34016 128 128 0q53.00224 0 90.50112 38.17472t37.49888 91.17696l0 468.66432q0 53.00224-37.49888 90.17344t-90.50112 37.1712l-768 0q-53.00224 0-90.50112-37.49888t-37.49888-90.50112l0-468.66432q0-53.00224 37.49888-90.8288t90.50112-37.82656l128 0zM512 341.34016q43.33568 0 82.8416 16.9984t67.9936 45.50656 45.50656 67.9936 16.9984 82.8416-16.9984 82.8416-45.50656 67.9936-67.9936 45.50656-82.8416 16.9984-82.8416-16.9984-67.9936-45.50656-45.50656-67.9936-16.9984-82.8416 16.9984-82.8416 45.50656-67.9936 67.9936-45.50656 82.8416-16.9984zM512 426.65984q-53.00224 0-90.50112 37.49888t-37.49888 90.50112 37.49888 90.50112 90.50112 37.49888 90.50112-37.49888 37.49888-90.50112-37.49888-90.50112-90.50112-37.49888zM722.3296 298.65984l-83.6608-128-251.65824 0-85.34016 128-173.6704 0q-17.67424 0-30.16704 12.67712t-12.4928 30.65856l0 468.66432q0 17.67424 12.4928 30.16704t30.16704 12.4928l768 0q18.00192 0 30.33088-12.16512t12.32896-29.83936l0-468.66432q0-18.00192-12.67712-31.00672t-30.0032-13.0048l-173.6704 0z"  ></path></symbol><symbol id="icon-mic" viewBox="0 0 1024 1024"><path d="M486.4 716.8C387.584 716.8 307.2 636.416 307.2 537.6v-307.2C307.2 131.584 387.584 51.2 486.4 51.2S665.6 131.584 665.6 230.4v307.2c0 98.816-80.384 179.2-179.2 179.2z m0-614.4A128.1536 128.1536 0 0 0 358.4 230.4v307.2C358.4 608.1536 415.8464 665.6 486.4 665.6s128-57.4464 128-128v-307.2C614.4 159.8464 556.9536 102.4 486.4 102.4z" fill="" ></path><path d="M819.2 537.6a25.6 25.6 0 0 0-51.2 0c0 155.2896-126.3104 281.6-281.6 281.6S204.8 692.8896 204.8 537.6a25.6 25.6 0 0 0-51.2 0 333.2608 333.2608 0 0 0 307.2 331.8272V972.8H384a25.6 25.6 0 0 0 0 51.2h204.8a25.6 25.6 0 0 0 0-51.2H512v-103.3728a333.2608 333.2608 0 0 0 307.2-331.8272z" fill="" ></path></symbol></svg>',
+            e = (t = document.getElementsByTagName("script"))[t.length - 1].getAttribute("data-injectcss");
+        if (e && !c.__iconfont__svg__cssinject__) {
+            c.__iconfont__svg__cssinject__ = !0;
+            try {
+                document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>")
+            } catch(t) {
+                console && console.log(t)
+            }
+        } !
+            function(t) {
+                if (document.addEventListener) if (~ ["complete", "loaded", "interactive"].indexOf(document.readyState)) setTimeout(t, 0);
+                else {
+                    var e = function() {
+                        document.removeEventListener("DOMContentLoaded", e, !1),
+                            t()
+                    };
+                    document.addEventListener("DOMContentLoaded", e, !1)
+                } else document.attachEvent && (o = t, i = c.document, l = !1, (a = function() {
+                    try {
+                        i.documentElement.doScroll("left")
+                    } catch(t) {
+                        return void setTimeout(a, 50)
+                    }
+                    n()
+                })(), i.onreadystatechange = function() {
+                    "complete" == i.readyState && (i.onreadystatechange = null, n())
+                });
+                function n() {
+                    l || (l = !0, o())
+                }
+                var o, i, l, a
+            } (function() {
+                var t, e; (t = document.createElement("div")).innerHTML = n,
+                    n = null,
+                (e = t.getElementsByTagName("svg")[0]) && (e.setAttribute("aria-hidden", "true"), e.style.position = "absolute", e.style.width = 0, e.style.height = 0, e.style.overflow = "hidden",
+                    function(t, e) {
+                        e.firstChild ?
+                            function(t, e) {
+                                e.parentNode.insertBefore(t, e)
+                            } (t, e.firstChild) : e.appendChild(t)
+                    } (e, document.body))
+            })
+    } (window);
